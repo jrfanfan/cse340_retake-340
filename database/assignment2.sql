8d0579bf9ad8b7acef9d3ef163a38819f7710e99
Fanfan <jrfanfan@yahoo.fr>
2023-12-11 13:12:05 -0500

    update

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -15,3 +15,20 @@ WHERE account_id = 1;
 UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
 WHERE inv_id = 10;
+
+-- INNER JOIN for tables `inventory` and `classification`
+SELECT
+	inv_make,
+	inv_model,
+	classification_name
+FROM
+	public.inventory
+INNER JOIN 	public.classification
+	ON public.inventory.classification_id = public.classification.classification_id
+	WHERE public.inventory.classification_id = 2;
+
+-- UPDATE for table `inventory`
+UPDATE public.inventory
+SET
+ inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'),
+ inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');
